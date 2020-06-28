95587a91d47241f061647673e0304292fa94b91a
timlo <a0987642936@yahoo.com>
2020-06-28 16:30:45 +0800

    Complet the authentication workflow

--- server/index.js
+++ server/index.js
@@ -1,11 +1,32 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
+const keys = require('./config/keys')
 
 const app = express()
+
+// console.developers.google.com
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientId,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken,refreshToken,profile,done) => {
+    console.log('accessToken: ',accessToken)
+    console.log('refreshToken: ',refreshToken)
+    console.log('profile: ',profile)
+}))
 
 
 app.get('/', (req, res) => {
     res.send({ 'bye': 'buddy' })
 })
+
+app.get('/auth/google',passport.authenticate('google',{
+    scope: ['profile','email']
+}))
+
+app.get('/auth/google/callback',passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
