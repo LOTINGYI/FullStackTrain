9b540aaa5db02a64e61870cf8739fd074086d334
timlo <a0987642936@yahoo.com>
2020-07-01 11:09:13 +0800

    Divide dev and prod environment

---
+++ server/config/keys.js
@@ -0,0 +1,8 @@
+// figure out what set of credentials to return
+if (process.env.NODE_ENV === 'production') {
+    // in production - return production keys
+    module.exports = require('./prod')
+} else {
+    // in development - return development keys
+    module.exports = require('./dev')
+}
--- server/index.js
+++ server/index.js
@@ -4,24 +4,24 @@ const cookieSession = require('cookie-session')
 const passport = require('passport')
 
 require('./models/User')
-// require('./services/passport') // We just ensure it executes so we don't need var
-// const keys = require('./config/keys')
-// mongoose.connect(keys.mongoURI)
+require('./services/passport') // We just ensure it executes so we don't need var
+const keys = require('./config/keys')
+mongoose.connect(keys.mongoURI)
 
 const app = express()
 
-// app.use(cookieSession({
-//     maxAge: 30 * 24 * 60 * 60 * 1000,
-//     keys: [keys.cookieKey]
-// }))
-// app.use(passport.initialize())
-// app.use(passport.session())
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get('/', (req, res) => {
     res.send({ 'bye': 'buddy' })
 })
 
-// require('./routes/authRoutes')(app);
+require('./routes/authRoutes')(app);
 
 
 const PORT = process.env.PORT || 5000
