a6026daaa4e1d6be5e660626e1b44821b5f2b3b6
timlo <a0987642936@yahoo.com>
2020-07-08 14:00:20 +0800

    Setup client side env and using redux to manage state

---
+++ server/client/src/reducers/authReducer.js
@@ -0,0 +1,10 @@
+import { FETCH_USER } from "../actions/types";
+export default function (state = null, action) {
+    switch (action.type) {
+        case FETCH_USER:
+            // tricky tech: '' || false => false, 'tim' || false => 'tim'
+            return action.payload || false; 
+        default:
+            return state;
+    }
+}
