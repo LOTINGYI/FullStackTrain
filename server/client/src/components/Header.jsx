b5c3f59cbec445089ed10bea5bcea5fe2dc6c1e3
timlo <a0987642936@yahoo.com>
2020-07-11 14:10:56 +0800

    Create billing in client

--- server/client/src/components/Header.jsx
+++ server/client/src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+
+import Payment from "./Payment";
 class Header extends Component {
     renderContent() {
         switch (this.props.auth) {
@@ -11,9 +13,11 @@ class Header extends Component {
                     <li><a href="/auth/google">Login With Google</a></li>
                 )
             default:
-                return (
-                    <li><a href="/api/logout">Logout</a></li>
-                )
+                return [
+                    <li key="1"><Payment /></li>,
+                    <li key="3" style={{margin:'0 10px'}}>Credits:{this.props.auth.credits}</li>,
+                    <li key="2"><a href="/api/logout">Logout</a></li>
+                ]
         }
     }
     render() {
@@ -21,7 +25,8 @@ class Header extends Component {
             <nav>
                 <div className="nav-wrapper">
                     <Link
-                        to={this.props.auth ? '/surveys': '/'}
+                        style={{margin: '0 10px'}}
+                        to={this.props.auth ? '/surveys' : '/'}
                         className="brand-logo">
                         TimLo
                     </Link>
